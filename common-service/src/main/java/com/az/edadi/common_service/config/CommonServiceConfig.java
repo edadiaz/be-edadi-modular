d787bcd5725513bb7030eb7cdaa7fd255dfa6a03
Ülvü Süleymanov <ulvu.suleyman.00@gmail.com>
2025-01-02 20:33:17 +0400

    add storage config

---
+++ common-service/src/main/java/com/az/edadi/common_service/config/CommonServiceConfig.java
@@ -0,0 +1,11 @@
+package com.az.edadi.common_service.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {
+        "com.az.edadi.common_service"
+})
+public class CommonServiceConfig {
+}
