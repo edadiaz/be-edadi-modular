6bbc77cf6945b3f4a1eff78b9f1d109fd9091190
Ülvü Süleymanov <ulvu.suleyman.00@gmail.com>
2025-02-09 00:45:15 +0400

    add configs

--- app/src/main/java/com/az/edadi/app/AppStarter.java
+++ app/src/main/java/com/az/edadi/app/AppStarter.java
@@ -3,10 +3,12 @@ package com.az.edadi.app;
 import com.az.edadi.app.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Import;
 
 @Import(AppConfig.class)
 @SpringBootApplication
+@EnableConfigServer
 public class AppStarter {
     public static void main(String[] args) {
         SpringApplication.run(AppStarter.class, args);
