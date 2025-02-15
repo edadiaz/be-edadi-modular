e9e22a41e40a5b01ef9fd73dcff75e2a721d5dff
Ülvü Süleymanov <ulvu.suleyman.00@gmail.com>
2025-02-16 01:11:46 +0400

    fix red underline on repositories

--- dal/src/main/java/com/az/edadi/dal/config/DalConfig.java
+++ dal/src/main/java/com/az/edadi/dal/config/DalConfig.java
@@ -1,11 +1,15 @@
 package com.az.edadi.dal.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.az.edadi.dal.repository")
 @EntityScan(basePackages = "com.az.edadi.dal.entity")
+@ComponentScan(basePackages = {
+        "com.az.edadi.dal"
+})
 public class DalConfig {
 }
