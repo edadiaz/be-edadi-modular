d38b10a91ac1f7651782273130b6ba5d30f2ccb8
Ülvü Süleymanov <ulvu.suleyman.00@gmail.com>
2025-03-15 05:26:56 +0400

    fix main

--- user/src/main/java/com/az/edadi/user/Main.java
+++ user/src/main/java/com/az/edadi/user/Main.java
@@ -1,6 +1,7 @@
 package com.az.edadi.user;
 
 import com.az.edadi.dal.repository.PostRepository;
+import com.az.edadi.dal.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class Main {
-    private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     @GetMapping("api/v1/test")
     public String test() {
         long start = System.currentTimeMillis();
 
 
-        return postRepository.findAll().toString()+" "+(System.currentTimeMillis()-start);
+        return userRepository.findAll().toString()+" "+(System.currentTimeMillis()-start);
      }
 }
