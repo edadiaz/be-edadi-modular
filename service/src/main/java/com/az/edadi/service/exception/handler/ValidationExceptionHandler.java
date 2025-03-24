eb8b215b790eb655ad2b22256f462813587ba465
Ülvü Süleymanov <ulvu.suleyman.00@gmail.com>
2025-03-24 12:31:23 +0400

    change error response

--- service/src/main/java/com/az/edadi/service/exception/handler/ValidationExceptionHandler.java
+++ service/src/main/java/com/az/edadi/service/exception/handler/ValidationExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,12 +16,18 @@ import java.util.Map;
 public class ValidationExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         BindingResult bindingResult = ex.getBindingResult();
-        Map<String, String> errors = new HashMap<>();
-        bindingResult.getFieldErrors().forEach(error ->
-                errors.put(error.getField(), Translator.getTranslation(error.getDefaultMessage()))
-        );
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now().toString());
+        response.put("status", String.valueOf(HttpStatus.BAD_REQUEST.value()));
+        var errors = bindingResult.getFieldErrors().stream().map(error ->
+                Map.of("field", error.getField(), "message", Translator.getTranslation(error.getDefaultMessage()))
+        ).toList();
+        response.put("errors",errors);
+//        bindingResult.getFieldErrors().forEach(error ->
+//                errors.put(error.getField(), Translator.getTranslation(error.getDefaultMessage()))
+//        );
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
